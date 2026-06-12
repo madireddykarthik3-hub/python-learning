20d5a0ef21dd4c48deb7579af1359d31b60ee6c7
madireddykarthik3-hub <madireddykarthik3@gmail.com>
2026-06-12 14:06:11 +0530

    day 6 : loops Qestion-2

--- day4.py
+++ day4.py
@@ -1,6 +1,15 @@
-age = int(input("Enter your age: "))
-
-if age >= 18:
-    print("You are eligible to vote")
+print ("enter the value of a")
+a = int(input())
+print("enter the value of b")
+b = int(input())
+print ("enter the value of c")
+c = int(input())
+if a == b and a == c:
+    print ("all the numbers are equal")
+elif a > b and a > c:
+    print ("a is the greatest")
+elif b > a and b > c:
+    print (" b is the gratest")
 else:
-    print("You are not eligible to vote")
+    print ("c is the greatest")
+    
---
+++ day6-Q2.py
@@ -0,0 +1,5 @@
+print ("the number for which you want the multiplication table:")
+num = int(input())
+for i in range(1 , 11):
+    print(num, "x", i, "=", num*i)
+    
