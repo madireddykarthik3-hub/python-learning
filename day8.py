bb65068406cc8f50ffa0b73389c1fd8dce149caa
madireddykarthik3-hub <madireddykarthik3@gmail.com>
2026-06-15 21:42:26 +0530

    day 8 star pattern generator

---
+++ day8.py
@@ -0,0 +1,9 @@
+print(" Star Pattern Generator")
+
+rows = int(input("Enter number of rows: "))
+
+for i in range(1, rows + 1):
+    print("*" * i)
+
+for i in range(rows - 1, 0, -1):
+    print("*" * i)
